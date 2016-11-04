44ec3a9ef4e6ba2aab31ee0bbc4b020a9100b615
Jonathan Campbell <jonathan@castus.tv>
2016-11-04 07:53:06 -0700

    my stable branch is upstream

--- user/joncampbell123/fetch-upstream.sh
+++ user/joncampbell123/fetch-upstream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add upstream https://github.com/open-watcom/open-watcom-v2.git || git remote set-url upstream https://github.com/open-watcom/open-watcom-v2.git || exit 1
+git remote add upstream https://github.com/joncampbell123/open-watcom-v2.git || git remote set-url upstream https://github.com/joncampbell123/open-watcom-v2.git || exit 1
 git fetch --all || exit 1
 
